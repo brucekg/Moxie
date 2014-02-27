0f7907d251f4d1400d8868e3c4ca8ac525487b09
Bruce K Gillespie <brucekg@fallenacorn.com>
2014-02-27 13:57:31 -0500

    stop creating job_id.  This is now done by SeaJob

--- src/Moxie/mock_jobs.py
+++ src/Moxie/mock_jobs.py
@@ -4,7 +4,6 @@ Created on Dec 17, 2013
 @author: brucekg
 '''
 
-import os.path
 import json
 
 class MockJobs(object):
@@ -28,19 +27,6 @@ class MockJobs(object):
         self.jobs = jobs
         self.reset()
         
-        fn = os.path.dirname(source)+'/MockJobs.json'
-        if os.path.exists(fn):
-            if os.path.isfile(fn):
-                data = open(fn, 'r').read()
-                self.data = json.loads(data)
-                self.data['job_id'] = int(self.data['job_id'])
-            else:
-                raise Exception("%s is not a file." % fn)
-        else:
-            self.data = {"job_id":3000}
-        
-        self.fn = fn
-
         return
     
     def reset(self):
@@ -50,16 +36,9 @@ class MockJobs(object):
     
     def next(self):
         
-        if self.data == None:
-            return "MockJobs - Jobs Not Loaded\n"
-        
         if self.index < len(self.jobs):
             d = self.jobs[self.index]
-            if d['job_id'] == '0':
-                d['job_id'] = str(self.data['job_id'])
-                self.data['job_id'] += 1
             r = json.dumps(d)
-            open(self.fn, 'w').write(json.dumps(self.data))
             self.index += 1
             self.none_count = 0
         else:
